26baee52ca312fa06ba33abe2969d350e85ce167
Ondřej Tulach <ondrej.tulach@gmail.com>
2019-01-11 07:35:45 +0100

    Kralíkos

--- src/ondrovyúkoly/OndrovyÚkoly.java
+++ src/ondrovyúkoly/OndrovyÚkoly.java
@@ -3,12 +3,15 @@ package ondrovyúkoly;
 public class OndrovyÚkoly {
 
     public static void main(String[] args) {
-        System.out.println("Násobilkovač!");
-        for (int a = 1; a <= 10; a++) {
-            for (int b = 1; b <= 10; b++) {
-                System.out.printf("%3d ", a * b);
-            }
-            System.out.println();
+        long[] fibonnanci = new long[20];
+        fibonnanci[0] = 1;
+        fibonnanci[1] = 1;
+        for (int i = 2; i < fibonnanci.length; i++) {
+            fibonnanci[i] = fibonnanci[i - 1] + fibonnanci[i - 2];
         }
+        for (long hodnota : fibonnanci) {
+            System.out.printf("%d ", hodnota);
+        }
+        System.out.println();
     }
 }
