6c016c66957c71b56f040c3c42af001bb37695b2
Ondřej Tulach <ondrej.tulach@gmail.com>
2019-01-14 18:06:51 +0100

    Ondrova šifra

--- src/ondrovyúkoly/OndrovyÚkoly.java
+++ src/ondrovyúkoly/OndrovyÚkoly.java
@@ -7,14 +7,17 @@ public class OndrovyÚkoly {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in, "UTF-8");
-        String[] tulachovi = {"Ondra", "Anna", "Jarda", "Sona"};
-        System.out.println("Zadej někoho z Tulachů");
-        String tulach = sc.nextLine();
-        Arrays.sort(tulachovi);
-        int pozice = Arrays.binarySearch(tulachovi, tulach);
-        if (pozice >= 0)
-        System.out.println("Je to Tulach!");
-else
-        System.out.println("Tohle není Tulach!");
+        System.out.println("Zadejte zprávu:");
+        String zprava = sc.nextLine();
+        String z = "";
+        System.out.println("Zadejte posun:");
+        int posun = sc.nextInt();
+        for (char c : zprava.toCharArray()) {
+            int i = (int) c;
+            i += posun;
+            char znak = (char) i;
+            z += znak;
         }
+        System.out.printf("Zakódovaná zpráva: %s\n", z);
     }
+}
